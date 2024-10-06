62ddad6fcb9651e9bee93d55fa066506c71ebb32
jjang-go <tykim9532@gmail.com>
2024-10-06 21:28:06 +0900

    11653 소인수분해

---
+++ baekjoon/11653/소인수분해.py
@@ -0,0 +1,10 @@
+from sys import stdin as si
+
+N = int(si.readline().rstrip())
+
+while N > 1:
+    for i in range(2, N+1):
+        if N % i == 0:
+            print(i)
+            N = int(N / i)
+            break
